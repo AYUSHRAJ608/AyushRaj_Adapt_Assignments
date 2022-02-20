74ab5f27def093ed945a6d776568237f86ea63d1
Ayush Raj <48960324+AYUSHRAJ608@users.noreply.github.com>
2022-02-20 09:53:52 +0530

    Completed Generics Assignment

---
+++ Core Java/GenericsAssignment/GenericsQ3.java
@@ -0,0 +1,25 @@
+class Generic<T>
+{
+	public T[] swap(T[] a, int i, int j) 
+	{
+        T temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        return a;
+	}
+	
+}
+public class GenericsQ3 
+{
+	public static void main(String[] args) 
+	{
+		Integer arr[] = {1,2,3,4,5,6,7,8,9};
+		Generic<Integer> objGeneric = new Generic<>();
+		Integer[] arr1=objGeneric.swap(arr, 1, 3);
+		for(int i : arr1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
