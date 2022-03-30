7b3fec20b0e0eac43f7577838aa379f4c671c7bc
Ayush Raj <48960324+AYUSHRAJ608@users.noreply.github.com>
2022-03-30 08:44:30 +0530

    Uploaded Spring security and Rest Assignment

---
+++ Spring Rest/Q2/assignmnet2/Assignmnet2.java
@@ -0,0 +1,14 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Assignmnet2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignmnet2.class, args);
+
+	}
+
+}
---
+++ Spring Rest/Q2/assignmnet2/UserService.java
@@ -0,0 +1,20 @@
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
+public class UserService
+{
+	public String validate(String username,String Password)
+	{
+        if (username.equals("Poonguzhali") && Password.equals("Hema"))
+        {
+            return "Valid User";
+        }
+        else
+        {
+        	return "Invalid User";
+        }
+    }
+
+}
